2e30d4e8729837432e5325ef5bbb09b6a6d59cbe
Paulina Noriega <humigym@gmail.com>
2015-05-30 20:17:02 -0700

    Email functionality added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var compress = require('compression');
 var app = express();
 var nodemailer = require('nodemailer');
 var validator = require('validator');
+
 var transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
@@ -14,6 +15,10 @@ var transporter = nodemailer.createTransport({
 	}
 });
 
+var validateMail = function(email, name, message){
+	return validator.isEmail(email) && name && name.length > 0 && message && message.length > 0;
+};
+
 var sendMail = function(email, name, message, tag) {
 	transporter.sendMail({
 		from: 'humigym@gmail.com',
@@ -21,7 +26,6 @@ var sendMail = function(email, name, message, tag) {
 		subject: tag + ', ' + name,
 		text: message + '\n' + email
 	});
-
 };
 
 app.use(compress());
@@ -30,12 +34,12 @@ app.use(express.static(__dirname + '/static'));
 var jsonParser = bodyParser.json();
 
 app.post('/sendMail', jsonParser, function(request, response) {
-	if (validator.isEmail(request.email) && validator.isAlpha(request.name) && request.message && request.tag) {
-		sendMail(request.email, request.name, request.message, request.tag);
+	if (validateMail(request.body.email, request.body.name, request.body.message)) {
+		sendMail(request.body.email, request.body.name, request.body.message, request.body.tag);
 		response.status('200');
-
 	} else {
 		response.status('400');
 	}
+	response.end();
 });
 app.listen(process.env.PORT || 3000);
--- static/js/humi.js
+++ static/js/humi.js
@@ -1,33 +1,27 @@
 var app = angular.module('humi', []);
 
 app.controller('ComentariosController', ['$scope', '$http', function($scope, $http) {
+	$scope.nombre = "";
+	$scope.mensaje = "";
+	$scope.email = "";
+	$scope.successful = false;
+
+	$scope.sendComment = function() {
+		$scope.successful = false;
+		$http.
+		post('/sendMail', {
+			"email": $scope.email,
+			"name": $scope.nombre,
+			"message": $scope.mensaje,
+		}).
+		success(function() {
+			$scope.successful = true;
 			$scope.nombre = "";
 			$scope.mensaje = "";
 			$scope.email = "";
-			$scope.tag = "";
+		}).
+		error(function() {
 			$scope.successful = false;
-			$scope.sendComment = function() {
-				$scope.successful = false;
-				$http.
-				post('/someUrl', {
-					"emai": $scope.nombre,
-					"name": $scope.mensaje,
-					"message": $scope.email,
-					"tag": $scope.tag
-				}).
-				success(function(status) {
-					if(status == 200){
-						$scope.successful = true;
-						$scope.nombre = "";
-						$scope.mensaje = "";
-						$scope.email = "";
-						$scope.tag = "";
-					}else{
-						$scope.successful = false;
-					}
-				}).
-				error(function(status) {
-					$scope.successful = false;
-				});
-			}
 		});
+	}
+}]);
